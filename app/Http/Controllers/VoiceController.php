a93508c778e552fbf9bf31aa430597a2e2867035
elitevinylwindows <aanif@elitevinylwindows.com>
2025-08-20 16:06:46 -0700

    Update VoiceController.php

--- app/Http/Controllers/VoiceController.php
+++ app/Http/Controllers/VoiceController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Google\Cloud\TextToSpeech\V1\TextToSpeechClient;
+use Google\Cloud\TextToSpeech\V1\Client\TextToSpeechClient;
 use App\Models\Voice;
 
 class VoiceController extends Controller
